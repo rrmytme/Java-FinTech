7c2ac941e9ab6d95166314e46212ae78edc12e81
Rajesh Rajappan <rrajeshrajappan@gmail.com>
2025-03-23 19:28:58 +1300

    Initial commit

---
+++ src/main/java/AutoTellerMachine.java
@@ -0,0 +1,6 @@
+public class AutoTellerMachine extends AccountOptions {
+    public static void main(String[] args) {
+        AccountOptions options = new AccountOptions();
+        options.getLogin();
+    }
+}
